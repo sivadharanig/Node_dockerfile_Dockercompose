a4be49423a488c989be2c94833e17f66107621bc
sivadharanig <109787932+sivadharanig@users.noreply.github.com>
2023-06-09 11:32:59 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,21 +1,21 @@
 //Build the Docker Image Using the Dockerfile in the root folder
 
-def buildImage() {
-    echo "Build Docker Image with Dockerfile..."
-    sh 'ls'
-    sh 'sudo docker --version'
-    sh 'docker build -t sivabics/demo-app:nodejs-app .'
-}
+// def buildImage() {
+//     echo "Build Docker Image with Dockerfile..."
+//     sh 'ls'
+//     sh 'sudo docker --version'
+//     sh 'docker build -t sivabics/demo-app:nodejs-app .'
+// }
 
 //Push Docker Image to DockerHub Repository
 
-def pushImage() {
-    echo "Pushing Docker Image to Docker Hub Repo..."
-    withCredentials([usernamePassword(credentialsId: '3c8dad21-0b06-4a37-88fd-252d1196c345', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push sivabics/demo-app:nodejs-app'
-    }
-} 
+// def pushImage() {
+//     echo "Pushing Docker Image to Docker Hub Repo..."
+//     withCredentials([usernamePassword(credentialsId: '3c8dad21-0b06-4a37-88fd-252d1196c345', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+//         sh "echo $PASS | docker login -u $USER --password-stdin"
+//         sh 'docker push sivabics/demo-app:nodejs-app'
+//     }
+// } 
 def deployImage() {
     echo "Deploying the application to EC2..."
     def dockerCmd = 'docker run -d -p 3000:5000 --name react-nodejs-app sivabics/demo-app:nodejs-app' 
